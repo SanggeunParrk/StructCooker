f56b6d3abf114c10ee940658e0dc300ad2cbcba5
SanggeunParrk <psk6950@snu.ac.kr>
2026-03-06 16:59:50 +0900

    fix remap_msa.py

--- remap_msa.py
+++ remap_msa.py
@@ -94,59 +94,59 @@ def load_bytes(byte_data: bytes) -> Mapping[str, Any]:
 
 
 def cp_dir(src:Path, dst:Path) -> None:
-	if not dst.exists():
-		dst.mkdir(parents=True)
-	os.system(f"cp -r {src} {dst}")
+    if not dst.exists():
+        dst.mkdir(parents=True)
+    os.system(f"rsync -av --exclude='*.hhr' --exclude='*.atab' {src}/ {dst}/")
 
 def load_seq_id_map(tsv_path:Path) -> dict:
-	seq_id_map = {}
-	with open(tsv_path, "r") as f:
-		for line in f:
-			line = line.strip()
-			if not line:
-				continue 
-			seq_id, seq = line.split("\t")
-			if not seq_id.startswith("P"):
-				continue
-			if seq in seq_id_map:
-				assert seq_id_map[seq] == seq_id, f"Duplicate sequence found: {seq}"
-				continue
-			if set(seq) == {"X"}:
-				continue
-			if len(seq) < 16:
-				continue
-			seq_id_map[seq] = seq_id
-	return seq_id_map
+    seq_id_map = {}
+    with open(tsv_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue 
+            seq_id, seq = line.split("\t")
+            if not seq_id.startswith("P"):
+                continue
+            if seq in seq_id_map:
+                assert seq_id_map[seq] == seq_id, f"Duplicate sequence found: {seq}"
+                continue
+            if set(seq) == {"X"}:
+                continue
+            if len(seq) < 16:
+                continue
+            seq_id_map[seq] = seq_id
+    return seq_id_map
 
 def load_query_seq_from_a3m(a3m_path:Path) -> str:
-	with open(a3m_path, "r") as f:
-		for line in f:
-			line = line.strip()
-			if line.startswith(">"):
-				continue
-			return line
-	return ""
+    with open(a3m_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith(">"):
+                continue
+            return line
+    return ""
 
 def main(seq_id_map_path: Path, old_msa_path: Path, new_msa_path: Path) -> None:
-	seq_id_map = load_seq_id_map(seq_id_map_path)
-	subdir_list = [d for d in old_msa_path.iterdir() if d.is_dir()]
-	not_found_count_list = []
-	found_list = []
-	for subdir in subdir_list:
-		old_msa_file = subdir / f"t000_msa0.a3m"
-		query_seq = load_query_seq_from_a3m(old_msa_file)
-		if query_seq not in seq_id_map:
-			# print(f"Query sequence not found in map: {query_seq}")
-			not_found_count_list.append(query_seq) # due to signal peptide
-			continue
-	
-		seq_id = seq_id_map[query_seq]
-		new_subdir = new_msa_path / seq_id[:4] / seq_id
-		# new_subdir.mkdir(parents=True, exist_ok=True)
-		# cp_dir(subdir, new_subdir)
-		# print(f"Copied {subdir} to {new_subdir}")
-		found_list.append(query_seq)
-	breakpoint()
+    seq_id_map = load_seq_id_map(seq_id_map_path)
+    subdir_list = [d for d in old_msa_path.iterdir() if d.is_dir()]
+    not_found_count_list = []
+    found_list = []
+    for subdir in subdir_list:
+        old_msa_file = subdir / f"t000_msa0.a3m"
+        query_seq = load_query_seq_from_a3m(old_msa_file)
+        if query_seq not in seq_id_map:
+            # print(f"Query sequence not found in map: {query_seq}")
+            not_found_count_list.append(query_seq) # due to signal peptide
+            continue
+    
+        seq_id = seq_id_map[query_seq]
+        new_subdir = new_msa_path / seq_id[:4] / seq_id
+        new_subdir.mkdir(parents=True, exist_ok=True)
+        cp_dir(subdir, new_subdir)
+        print(f"Copied {subdir} to {new_subdir}")
+        found_list.append(query_seq)
+    breakpoint()
 
 def len_lmdb(lmdb_path: Path) -> int:
     env = lmdb.open(
@@ -160,42 +160,38 @@ def len_lmdb(lmdb_path: Path) -> int:
     with env.begin() as txn:
         stat = txn.stat()
         return stat["entries"]
-	
+    
 def load_item_from_lmdb(lmdb_path:Path, key_idx: int) -> tuple:
-	env = lmdb.open(
-		str(lmdb_path),
-		readonly=True,
-		lock=False,
-		readahead=False,
-		max_readers=1
-	)
+    env = lmdb.open(
+        str(lmdb_path),
+        readonly=True,
+        lock=False,
+        readahead=False,
+        max_readers=1
+    )
 
-	with env.begin() as txn:
-		cursor = txn.cursor()
-		for idx, (key, value) in enumerate(cursor):
-			if idx == key_idx:
-				value = load_bytes(bytes(value))
-				msa_depth = value['msa_container']['residue_container']['nodes']['sequences']['value'].shape[1]
-				return key.decode(), value, msa_depth
-	return None, None, None
+    with env.begin() as txn:
+        cursor = txn.cursor()
+        for idx, (key, value) in enumerate(cursor):
+            if idx == key_idx:
+                value = load_bytes(bytes(value))
+                msa_depth = value['msa_container']['residue_container']['nodes']['sequences']['value'].shape[1]
+                return key.decode(), value, msa_depth
+    return None, None, None
 
 def num_found(msa_db_path: Path) -> int:
-	total_count = len_lmdb(msa_db_path)
-	found_count = 0
-	for idx in range(total_count):
-		key, value, msa_depth = load_item_from_lmdb(msa_db_path, idx)
-		if key is None:
-			continue
-		if msa_depth > 0:
-			found_count += 1
-	return found_count
+    total_count = len_lmdb(msa_db_path)
+    found_count = 0
+    for idx in range(total_count):
+        key, value, msa_depth = load_item_from_lmdb(msa_db_path, idx)
+        if key is None:
+            continue
+        if msa_depth > 0:
+            found_count += 1
+    return found_count
 
 if __name__ == "__main__":
-	seq_id_map_path = Path("/public_data02/BioMolDB_20260224/metadata/seq_id_map.tsv")
-	old_msa_path = Path("/public_data02/old_msa/new_hash_a3m")
-	new_msa_path = Path("/public_data02/msa/")	
-	old_msa_db_path = Path("/public_data/BioMolDBv2_2024Oct21/a3m.lmdb")
-	msa_in_db = len_lmdb(old_msa_db_path)
-	num_found_in_db = num_found(old_msa_db_path)
-	breakpoint()
-	main(seq_id_map_path, old_msa_path, new_msa_path)
+    seq_id_map_path = Path("/data/psk6950/BioMolDB_20260224/metadata/seq_id_map.tsv")
+    old_msa_path = Path("/data/psk6950/MSA/new_hash_a3m")
+    new_msa_path = Path("/data/psk6950/msa/")	
+    main(seq_id_map_path, old_msa_path, new_msa_path)
