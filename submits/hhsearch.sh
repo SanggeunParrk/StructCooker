612067926a43d9bb939fc7618492e4ccaff779cd
SanggeunParrk <psk6950@snu.ac.kr>
2026-03-16 23:57:51 +0900

    add: hhsearch

--- submits/hhsearch.sh
+++ submits/hhsearch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH -J hhsearch
-#SBATCH --nodes=1
+#SBATCH --nodes=12
 #SBATCH --ntasks-per-node=1
 #SBATCH -c 112
 #SBATCH --mem=491g
-#SBATCH -p cpu-standard
-#SBATCH --qos=cpu-standard-q
+#SBATCH -p cpu-long
+#SBATCH --qos=cpu-long-q
 #SBATCH -o ./logs/hhsearch.out
 #SBATCH -e ./logs/hhsearch.err
 
-hhsearch -b 50 -B 500 -z 50 -Z 500 -mact 0.05 -cpu 4 -maxmem 16 -aliw 100000 -e 100 -p 5.0 -d "/data/psk6950/template_db/pdb100_20260224/" \
-	-i "/data/psk6950/msa/P000/P0003600/P0003600.a3m" -o "/data/psk6950/msa/P000/P0003600/P0003600.hhr" -atab "/data/psk6950/msa/P000/P0003600/P0003600.atam" -v 0
+export PYTHONPATH="."
+export OMP_NUM_THREADS="${SLURM_CPUS_PER_TASK}"
+
+srun --ntasks="${SLURM_NTASKS}" --ntasks-per-node=1 --cpus-per-task="${SLURM_CPUS_PER_TASK}" \
+  python -m scripts.postprocess data_transform_parallel "configs/hhsearch.yaml"
