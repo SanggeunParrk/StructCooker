2dfd8df31667fa665475186906f6af4317822394
SanggeunParrk <psk6950@snu.ac.kr>
2026-03-20 14:44:25 +0900

    continue

---
+++ submits/hmmsearch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J hmmsearch
+#SBATCH --nodes=12
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 112
+#SBATCH --mem=491g
+#SBATCH -p cpu-long
+#SBATCH --qos=cpu-long-q
+#SBATCH -o ./logs/hmmsearch.out
+#SBATCH -e ./logs/hmmsearch.err
+
+export PYTHONPATH="."
+export OMP_NUM_THREADS="${SLURM_CPUS_PER_TASK}"
+
+srun --ntasks="${SLURM_NTASKS}" --ntasks-per-node=1 --cpus-per-task="${SLURM_CPUS_PER_TASK}" \
+  python -m scripts.postprocess data_transform_parallel "configs/hmmsearch.yaml"
