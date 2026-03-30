b9b3f524ab6c8ebbe94c467982eb65ccf75d63ee
SanggeunParrk <psk6950@snu.ac.kr>
2026-03-30 18:06:52 +0900

    continue

--- scripts/build_lmdb.py
+++ scripts/build_lmdb.py
@@ -78,17 +78,19 @@ def build(
             msg = f"Invalid shard index {shard_idx} for {n_shards} shards."
             raise click.BadParameter(msg)
         # Split CIF list into n_shards and take only the shard_idx part
-        data_list = [
-            data for i, data in enumerate(data_list) if i % n_shards == shard_idx
+        filtered_data_list = [
+            data
+            for i, data in enumerate(filtered_data_list)
+            if i % n_shards == shard_idx
         ]
         click.echo(
-            f"Processing shard {shard_idx}/{n_shards} with {len(data_list)} files.",
+            f"Processing shard {shard_idx}/{n_shards} with {len(filtered_data_list)} files.",
         )
         config_dict["env_path"] = Path(config_dict["env_path"]).with_name(
             f"{Path(config_dict['env_path']).stem}_shard{shard_idx}{Path(config_dict['env_path']).suffix}",
         )
     else:
-        click.echo(f"Processing all {len(data_list)} files as a single shard.")
+        click.echo(f"Processing all {len(filtered_data_list)} files as a single shard.")
 
     build_lmdb(*filtered_data_list, **config_dict, map_size=map_size)
 
